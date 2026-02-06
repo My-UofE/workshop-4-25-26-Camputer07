f530abcb12750c2177614af408529cd861196ac9
Camputer07 <chr215@exeter.ac.uk>
2026-02-06 18:01:28 +0000

    Completed part 2.

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,7 @@
 // A Rectangle class
+
+import java.nio.ReadOnlyBufferException;
+
 public class Rectangle {
   
   // 4 instance attributes
@@ -44,4 +47,49 @@ public class Rectangle {
     return 2 * (width + height);
   }
   
+  // Method: scale width and height separately
+  public void scale(double scaleX, double scaleY) {
+    // Scale the width and height individually
+    this.width *= scaleX;
+    this.height *= scaleY;
+  }
+
+  // Method: scale width and height together
+  public void scale(double scale) {
+    // Scale the width and height by the same amount
+    this.width *= scale;
+    this.height *= scale;
+  }
+
+  // Method: checks if two rectangles are overlapping
+  public boolean isOverlappedWith(Rectangle r) {
+    // Check if the two rectangles are overlapping in the x direction
+    boolean xOverlap = (Math.abs(r.originX - this.originX)) < ((this.width / 2) + (r.width / 2));
+    
+    // Check if the two rectangles are overlapping in the y direction
+    boolean yOverlap = (Math.abs(r.originY - this.originY)) < ((this.height / 2) + (r.height / 2));
+
+    // For the two rectangles to be overlapping, they must overlap in the x and y direction
+    return xOverlap && yOverlap;
+  }
+
+  // Method: checks if two rectangles are overlapping
+  public static boolean areOverlapping(Rectangle r1, Rectangle r2) {
+    // Use the above function to reduce code duplication
+    return r1.isOverlappedWith(r2);
+  }
+
+  // Method: calculate the aspect ratio of the rectangle
+  public double calcRatio(double width, double height) {
+    return width / height;
+  }
+
+  // Method: determine if this rectangle is square
+  public boolean isSquare() {
+    // Calculate the aspect ratio of this rectangle
+    double aspectRatio = calcRatio(this.width, this.height);
+
+    // If this is within a treshold range, it is square
+    return (0.999999 < aspectRatio) && (aspectRatio < 1.000001);
+  }
 }
